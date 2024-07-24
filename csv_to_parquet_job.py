b8d22d42a26fd16e20e421016741f34f6df20ab0
Bency2712 <bencythomas6645@gmail.com>
2024-07-23 19:11:45 -0700

    Added Architecture

---
+++ csv_to_parquet_job.py
@@ -0,0 +1,41 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+from awsglue.dynamicframe import DynamicFrame
+
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+predicate_pushdown = "region in ('ca','gb','us')"
+
+#Data Source
+DataSource0= glueContext.create_dynamic_frame.from_catalog(database = "ben-youtube-raw", table_name = "raw_statistics", transformation_ctx = "datasource0", push_down_predicate = predicate_pushdown)
+
+#Mapping
+TableMapping = ApplyMapping.apply(frame=DataSource0, mappings=[("video_id", "string", "video_id", "string"), ("trending_date", "string", "trending_date", "string"), ("title", "string", "title", "string"), ("channel_title", "string", "channel_title", "string"), ("category_id", "long", "category_id", "long"), ("publish_time", "string", "publish_time", "string"), ("tags", "string", "tags", "string"), ("views", "long", "views", "long"), ("likes", "long", "likes", "long"), ("dislikes", "long", "dislikes", "long"), ("comment_count", "long", "comment_count", "long"), ("thumbnail_link", "string", "thumbnail_link", "string"), ("comments_disabled", "boolean", "comments_disabled", "boolean"), ("ratings_disabled", "boolean", "ratings_disabled", "boolean"), ("video_error_or_removed", "boolean", "video_error_or_removed", "boolean"), ("description", "string", "description", "string"), ("region", "string", "region", "string")], transformation_ctx="TableMapping")
+
+resolvechoice2 = ResolveChoice.apply(frame = TableMapping, choice = "make_struct", transformation_ctx = "resolvechoice2")
+    
+dropnullfields3 = DropNullFields.apply(frame = resolvechoice2, transformation_ctx = "dropnullfields3")
+
+
+datasink1 = dropnullfields3.toDF().coalesce(1)
+print("Record count: ", datasink1.count())
+
+df_final_output = DynamicFrame.fromDF(datasink1, glueContext, "df_final_output")
+datasink4 = glueContext.write_dynamic_frame.from_options(frame = df_final_output, connection_type = "s3", connection_options = {"path": "s3://ben-youtube-cleansed-useast2-dev/youtube/raw_statistics/", "partitionKeys": ["region"]}, format = "parquet", transformation_ctx = "datasink4")
+
+job.commit()
